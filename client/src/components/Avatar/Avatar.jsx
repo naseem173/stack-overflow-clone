afd8ce1846b46cc50980a687659a4fbf3ae589af
ABC <naseem_md@outlook.com>
2023-05-07 13:55:03 +0530

    Created login and Sign up page

--- client/src/components/Avatar/Avatar.jsx
+++ client/src/components/Avatar/Avatar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Avatar = () => {
+const Avatar = ({ children, backgroundColor, padding, color, borderRadius, fontSize, cursor}) => {
+
+    const style ={
+      backgroundColor,
+      padding: '${py} ${px}',
+      color: color || 'black',
+      borderRadius,
+      fontSize,
+      textAlign:"center",
+      cursor : cursor || null,
+      textDecoderation : "none"
+    }
   return (
-    <div>
-      
+    <div style={style}>
+        { children}
     </div>
   )
 }
